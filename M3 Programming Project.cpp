f9643cae9c273b1478428f9971171f5177e4db89
Malakye Brown <prophetoftruth07@gmail.com>
2026-03-19 18:03:06 -0400

    Mutators set up

--- M3 Programming Project.cpp
+++ M3 Programming Project.cpp
@@ -12,20 +12,40 @@ public:
     Date(int m, int d, int y) : month(m), day(d), year(y) {}
 
     void setMonth(int m) {
-        if (m > 0 && m < 12) month = m;
+        if (m > 0 && m < 12) {
+            month = m;
+        }
+        else {
+            m = 1;
+            month = m;
+        }
     }
 
     void setYear(int y) {
-        if (y > -1) year = y;
+        if (y > -1) {
+            year = y;
+        }
+        else {
+            y = 1900;
+            year = y;
+        }
     }
 
     void setDay(int d) {
-        if ((month == 4 || month == 6 || month == 9 || month == 11) && (d > 31 && d < 0)) day = d;
-        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (d > 32 && d < 0)) day = d;
-        if (month == 2 && (d > 28 && d < 0)) day = d;
+        if ((month == 4 || month == 6 || month == 9 || month == 11) && (d > 31 && d < 0)) {
+            day = d;
+        }
+        else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (d > 32 && d < 0)) {
+            day = d;
+        }
+        else if (month == 2 && year % 4 == 0 && (d > 28 && d < 0)) {
+            day = d;
+        }
+        else {
+            d = 1;
+            day = d;
+        }
    }
-
-    void setDate(int m, int d, int y);
 };
 
 int main()

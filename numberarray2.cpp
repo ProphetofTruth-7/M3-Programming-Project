fc37d6830da6e02f8d0bc464bd97a46e2322c1df
Malakye Brown <prophetoftruth07@gmail.com>
2026-03-29 15:51:46 -0400

    Checkpoint to go figure out how to dynamically allocate again

--- numberarray2.cpp
+++ numberarray2.cpp
@@ -1,2 +1,17 @@
 #include "numberarray2.h"
+#include <string>
+using namespace std;
 
+NumberArray::NumberArray(int s, double* d) : size(s), data(d){}
+
+void Date::setMonth(int m) {
+    if (m > 0 && m < 13) {
+        month = m;
+        setMonthName();
+    }
+    else {
+        m = 1;
+        month = m;
+        setMonthName();
+    }
+}
--- numberarray2.h
+++ numberarray2.h
@@ -1,7 +1,36 @@
 #ifndef NUMBERARRAY2_H
 #define NUMBERARRAY2_H
+using namespace std;
 
+// This is the Header File, containing Prototypes for all Member Function and the Class itself //
+class NumberArray {
+private:
+    int size;
+    double* data;
+public:
+    // Constructor //
+    NumberArray(int s, double* d);
 
+    // Function Prototypes/Documentation //
+ /*
+ * setMonth
+ * ------------------------------------------------------------
+ * Sets the variable month to default value unless given a valid value(1-12)
+ *
+ * Inputs:
+ *   m              - A integer variable given directly or via User input
+ * Outputs:
+ *   month          - A valid integer that corresponds to a calendar. If given input was invalid, resorts to default given by Constructor
+ *   setMonthname   - A function called that gives the name of the given integer or its resorted default
+ *
+ * Preconditions:
+ *   - Contructor Default exists and is itself valid
+ *
+ * Postconditions:
+ *   - month is either a valid integer corresponding to the calendar or the default value
+ *   - setMonthName is ran immediately following setMonth
+ */
+    void setMonth(int m);
 
 #endif
 
